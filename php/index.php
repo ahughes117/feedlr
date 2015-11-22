ccecedb2bed5e9ea88cf285c6595952c237628c2
Alex Hughes <ahughes@ahughes.org>
2015-11-22 16:53:16 +0000

    Forgot to add a title to the index page

--- php/index.php
+++ php/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Feedlr</title>
     </head>
     <body>
         Proudly powered by <a href='https://github.com/ahughes117/feedlr'>feedlr</a>. So far it tends to do a good job.
